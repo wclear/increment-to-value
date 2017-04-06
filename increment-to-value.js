5ae8ca1acc2385026d6f8cd6a9cb1f3cd32ce075
William Clear <william.j.clear@gmail.com>
2017-04-06 15:30:53 +0200

    Add starting times, handle cases where figure increments in reverse

--- increment-to-value.js
+++ increment-to-value.js
@@ -7,9 +7,44 @@
 		return runningTime > 0 ? runningTime : 1
 	}
 
+	// Gets an integer for the start value
+	function getValidStartValue(target) {
+		var startValue = parseInt(target.getAttribute('data-start-value'));
+		if (isNaN(startValue)) {
+			return 0;
+		}
+		else {
+			return startValue;
+		}
+	}
+
+	// Initialises all the meta information required for each counter
+	function initMeta(targets) {
+		var meta = [];
+		for (var i = 0; i < targets.length; i++) {
+			var finalValueStr = targets[i].getAttribute('data-final-value'),
+				startValueTemp = getValidStartValue(targets[i]),
+				finalValueTemp = parseInt(finalValueStr);
+			var metaObj = {
+				startValue: startValueTemp,
+				finalValueStrLength: finalValueStr.length,
+				finalValue: finalValueTemp,
+				runningTime: getValidRunningTime(targets[i]),
+				reverse: startValueTemp > finalValueTemp,
+				range: startValueTemp - finalValueTemp,
+				completed: false,
+				target: targets[i]
+			}
+			meta.push(metaObj);
+			targets[i].childNodes[0].textContent = metaObj.startValue;
+		}
+		return meta;
+	}
+
 	var frameTime = 19, // Number of milliseconds to wait for the next frame
 		startTime = +(new Date()),
 		targets = document.querySelectorAll('.increment-to-value'),
+		targetsMeta = initMeta(targets),
 		pads = [
 			'',
 			'0',
@@ -24,46 +59,42 @@
 			'0000000000'
 		];
 
-	// Set all counters to their initial values.
-	for (var i=targets.length-1; i>=0; i--) {
-		targets[i].childNodes[0].textContent = "0";
-		targets[i].incrementToValue = {
-			finalValueStrLength: targets[i].getAttribute('data-final-value').length,
-			finalValue: parseInt(targets[i].getAttribute('data-final-value')),
-			runningTime: getValidRunningTime(targets[i]),
-			completed: false
-		}
-	}
 
 	// Run a timer until all counters are finished.
 	var timerId = window.setInterval(function() {
 		var finishedCount = 0;
-		for (var i = targets.length - 1; i >= 0; i--) {
+		for (var i = targetsMeta.length - 1; i >= 0; i--) {
 
 			// Skip targets that have already reached their final value
-			if (targets[i].incrementToValue.completed) {
+			if (targetsMeta[i].completed) {
 				continue;
 			}
 
 			// The new value for the counter is the % of the way through the running time (now - startTime / runningTime) multiplied by the final value
-			var newValue = parseInt(targets[i].incrementToValue.finalValue * (+(new Date()) - startTime) / targets[i].incrementToValue.runningTime);
+			var percentage = (+(new Date()) - startTime) / targetsMeta[i].runningTime,
+				newValue = parseInt(targetsMeta[i].startValue - (percentage * targetsMeta[i].range));
 
 			// Check if we've reached the final value on this frame
-			if (newValue >= targets[i].incrementToValue.finalValue) {
+			if ((targetsMeta[i].reverse && newValue <= targetsMeta[i].finalValue) || (!targetsMeta[i].reverse && newValue >= targetsMeta[i].finalValue)) {
 
 				// And finalise it if so
-				newValue = targets[i].incrementToValue.finalValue;
-				targets[i].incrementToValue.completed = true;
+				newValue = targetsMeta[i].finalValue;
+				targetsMeta[i].completed = true;
 				++finishedCount;
 			}
 
 			// Update the text to reflect the current increment
-			var newText = parseInt("1" + pads[targets[i].incrementToValue.finalValueStrLength - (newValue + "").length] + newValue).toLocaleString();
-			targets[i].childNodes[0].textContent = newText.indexOf(",") === 1 ? newText.substring(2) : newText.substring(1);
+			if (targetsMeta[i].reverse) {
+				targetsMeta[i].target.childNodes[0].textContent = newValue.toLocaleString();
+			}
+			else {
+				var newText = parseInt("1" + pads[targetsMeta[i].finalValueStrLength - (newValue + "").length] + newValue).toLocaleString();
+				targetsMeta[i].target.childNodes[0].textContent = newText.indexOf(",") === 1 ? newText.substring(2) : newText.substring(1);
+			}
 		}
 
 		// Stop running the interval timer if all counters have finished incrementing.
-		if (finishedCount === targets.length) {
+		if (finishedCount === targetsMeta.length) {
 			window.clearInterval(timerId);
 		}
 
